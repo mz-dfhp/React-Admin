3af2039d898acad6ddba4ad983fe1f0a8910a26d
mz <486836332@qq.com>
2022-11-04 23:45:18 +0800

    feat: add eslint + prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import viteEslint from 'vite-plugin-eslint'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    viteEslint({
+      failOnError: true
+    })
+  ]
 })
