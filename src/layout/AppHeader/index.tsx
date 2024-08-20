43503281ec7e3b9b2a815e5de4f617cba10e1f10
mz <486836332@qq.com>
2024-08-20 18:47:01 +0800

    feat: update

--- src/layout/AppHeader/index.tsx
+++ src/layout/AppHeader/index.tsx
@@ -31,7 +31,7 @@ export default function AppHeader({ collapsed, setCollapsed }: { collapsed: bool
         className={`cursor-pointer  text-[18px] ${`${collapsed ? 'icon-[bi--text-indent-left]' : 'icon-[bi--text-indent-right]'}`}`}
         onClick={() => setCollapsed(!collapsed)}
       />
-      <div className="ml-auto flex items-center justify-around px-[25px] w-0 overflow-hidden md:w-auto md:overflow-visible">
+      <div className="ml-auto flex items-center justify-around w-0 overflow-hidden md:w-auto md:overflow-visible md-px-[25px]">
         <Tooltip title={locale}>
           <div
             className="icon-[bi--arrow-left-right] ml-[20px] cursor-pointer transition-all hover:scale-[1.2]"
